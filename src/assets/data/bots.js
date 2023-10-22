eff0036564aa5b8856ad708a839d9d9ff9c9dff1
Derry-ukere <ukderry@gmail.com>
2023-10-22 18:46:54 +0100

    added prices

---
+++ src/assets/data/bots.js
@@ -0,0 +1,42 @@
+// export const softwares = [
+//     {
+//         name : "MegaTX-004",
+//         image : "https://www.searchenginejournal.com/wp-content/uploads/2020/06/47e5b89a-2b1c-4dcd-a9a8-5db0118157cb-5efbf892d137a.jpeg",
+//         description : "https://www.searchenginejournal.com/wp-content/uploads/2020/06/47e5b89a-2b1c-4dcd-a9a8-5db0118157cb-5efbf892d137a.jpeg",
+//         exchanges : ["binance", "springindex","megacap","magdowncap"],
+//         cost : '$1900',
+//         version: '1:20',
+//         backtestingResults : '1222/222'
+
+//     }
+// ]
+
+
+// Software Name: MyTradingBot Enhancer
+// Description: A versatile tool to boost your trading bot's performance with advanced analytics and risk management features.
+// Vendor/Developer: XYZ Software Co.
+// Compatibility: Supports TradingBot 2.0 and higher
+// Version Number: 3.1.0
+// Pricing Information: $199 for a one-year license
+// Configuration Requirements: Windows 10 or later, 4GB RAM, 100GB free disk space
+// Installation Guide: [Link to Installation Guide]
+// User Manual: [Link to User Manual]
+// Support Information: support@xyzsoftware.com
+// Release Date: 2023-10-15
+// Category/Tags: Trading Strategy, Analytics, Risk Management
+// Screenshots or Demo Video: [Link to Screenshots]
+// Trial Version: Available for 14 days
+// License Key Generation: [Generate License Key]
+// File Upload: [Upload Software Package]
+// Security Features: Two-factor authentication, data encryption
+// Reviews and Ratings: ★★★★☆ (4.3/5)
+// Integration Options: REST API, WebSockets
+// Terms and Conditions: [Link to Terms and Conditions]
+// API Access:
+//    - API Type: REST
+//    - API Key: [API Key]
+//    - API Documentation: [Link to API Documentation]
+// Performance Metrics and Benchmarks:
+//    - Historical ROI: 30% over the last year
+//    - Benchmark Comparisons: Outperforms S&P 500 by 10%
+//    - Backtesting Results: $5,000 profit in the last 6 months
